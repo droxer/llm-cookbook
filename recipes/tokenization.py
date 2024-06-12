2dcf1afb1b4c0045dffe08de87d46f0bc43b4bc1
He Fei <droxer.he@gmail.com>
2024-06-12 23:29:08 +0800

    add tokenization.

---
+++ recipes/tokenization.py
@@ -0,0 +1,13 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+sequence = "Using a Transformer network is simple"
+tokens = tokenizer.tokenize(sequence)
+
+print(tokens)
+
+
+ids = tokenizer.convert_tokens_to_ids(tokens)
+
+print(ids)

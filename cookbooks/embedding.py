86883bd23f21dc02a22626e671d7db13598c2592
He Fei <droxer.he@gmail.com>
2025-05-13 18:34:59 +0800

    Updated models.

---
+++ cookbooks/embedding.py
@@ -0,0 +1,104 @@
+from sentence_transformers import SentenceTransformer, util
+from rich import print as rprint
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedder = SentenceTransformer(
+    "BAAI/bge-large-zh-v1.5",
+    # "jinaai/jina-embeddings-v3",
+    trust_remote_code=True,
+)
+
+# Corpus with example sentences
+corpus = [
+    "A man is eating food.",
+    "A man is eating a piece of bread.",
+    "The girl is carrying a baby.",
+    "A man is riding a horse.",
+    "A woman is playing violin.",
+    "Two men pushed carts through the woods.",
+    "A man is riding a white horse on an enclosed ground.",
+    "A monkey is playing drums.",
+    "A cheetah is running behind its prey.",
+]
+
+queries = [
+    "A man is eating pasta.",
+    "Someone in a gorilla costume is playing a set of drums.",
+    "A cheetah chases prey on across a field.",
+]
+
+
+# Use "convert_to_tensor=True" to keep the tensors on GPU (if available)
+corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+
+corpus_embeddings = corpus_embeddings.to("mps")
+corpus_embeddings = util.normalize_embeddings(corpus_embeddings)
+
+
+for query in queries:
+    query_embedding = embedder.encode(query, convert_to_tensor=True)
+    rprint("\nQuery:", query)
+    rprint("Top 5 most similar sentences in corpus:")
+
+    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
+    hits = hits[0]  # Get the hits for the first query
+    for hit in hits:
+        rprint(corpus[hit["corpus_id"]], "(Score: {:.4f})".format(hit["score"]))
+
+
+# 使用 jinaai/jina-embeddings-v3模型
+"""
+Query: A man is eating pasta.
+Top 5 most similar sentences in corpus:
+A man is eating food. (Score: 0.8442)
+A man is eating a piece of bread. (Score: 0.7457)
+A man is riding a horse. (Score: 0.4423)
+A monkey is playing drums. (Score: 0.3901)
+A man is riding a white horse on an enclosed ground. (Score: 0.3779)
+
+Query: Someone in a gorilla costume is playing a set of drums.
+Top 5 most similar sentences in corpus:
+A monkey is playing drums. (Score: 0.8406)
+A man is riding a horse. (Score: 0.5040)
+A woman is playing violin. (Score: 0.4966)
+A cheetah is running behind its prey. (Score: 0.4669)
+A man is riding a white horse on an enclosed ground. (Score: 0.4631)
+
+Query: A cheetah chases prey on across a field.
+Top 5 most similar sentences in corpus:
+A cheetah is running behind its prey. (Score: 0.9227)
+A monkey is playing drums. (Score: 0.5145)
+A man is riding a white horse on an enclosed ground. (Score: 0.4276)
+Two men pushed carts through the woods. (Score: 0.4155)
+A man is riding a horse. (Score: 0.4144)
+"""
+
+# 使用 BAAI/bge-large-zh-v1.5 模型
+"""
+Query: A man is eating pasta.
+Top 5 most similar sentences in corpus:
+A man is eating food. (Score: 0.8750)
+A man is eating a piece of bread. (Score: 0.8173)
+A man is riding a horse. (Score: 0.6221)
+A man is riding a white horse on an enclosed ground. (Score: 0.5911)
+A woman is playing violin. (Score: 0.4883)
+
+Query: Someone in a gorilla costume is playing a set of drums.
+Top 5 most similar sentences in corpus:
+A monkey is playing drums. (Score: 0.6211)
+A woman is playing violin. (Score: 0.5694)
+A man is riding a white horse on an enclosed ground. (Score: 0.5404)
+A man is riding a horse. (Score: 0.5376)
+A man is eating a piece of bread. (Score: 0.5231)
+
+Query: A cheetah chases prey on across a field.
+Top 5 most similar sentences in corpus:
+A cheetah is running behind its prey. (Score: 0.8325)
+Two men pushed carts through the woods. (Score: 0.6193)
+A monkey is playing drums. (Score: 0.5686)
+A man is riding a horse. (Score: 0.5437)
+A man is riding a white horse on an enclosed ground. (Score: 0.5280)
+"""
--- cookbooks/reranking.py
+++ cookbooks/reranking.py
@@ -2,9 +2,9 @@ from sentence_transformers import CrossEncoder
 from rich import print as rprint
 
 model = CrossEncoder(
-    "jinaai/jina-reranker-v2-base-multilingual",
-    # "BAAI/bge-reranker-v2-m3",
-    automodel_args={"torch_dtype": "auto"},
+    "BAAI/bge-reranker-v2-m3",
+    # "jinaai/jina-reranker-v2-base-multilingual",
+    model_kwargs={"torch_dtype": "auto"},
     trust_remote_code=True,
 )
 
@@ -37,16 +37,33 @@ for ranking in rankings:
     rprint(
         f"ID: {ranking['corpus_id']}, Score: {ranking['score']:.4f}, Text: {ranking['text']}"
     )
+
+# 使用 jinaai/jina-reranker-v2-base-multilingual 模型
 """
 Query: Organic skincare products for sensitive skin
 ID: 6, Score: 0.9258, Text: 针对敏感肌专门设计的天然有机护肤产品
 ID: 8, Score: 0.8438, Text: 敏感肌のために特別に設計された天然有機スキンケア製品
 ID: 0, Score: 0.8281, Text: Organic skincare for sensitive skin with aloe vera and chamomile.
 ID: 4, Score: 0.7617, Text: Cuidado de la piel orgánico para piel sensible con aloe vera y manzanilla
-ID: 2, Score: 0.6328, Text: Bio-Hautpflege für empfindliche Haut mit Aloe Vera und Kamille
-ID: 9, Score: 0.1113, Text: 新しいメイクのトレンドは鮮やかな色と革新的な技術に焦点を当てています
+ID: 2, Score: 0.6289, Text: Bio-Hautpflege für empfindliche Haut mit Aloe Vera und Kamille
+ID: 9, Score: 0.1128, Text: 新しいメイクのトレンドは鮮やかな色と革新的な技術に焦点を当てています
 ID: 5, Score: 0.0996, Text: Las nuevas tendencias de maquillaje se centran en colores vivos y técnicas innovadoras
-ID: 1, Score: 0.0928, Text: New makeup trends focus on bold colors and innovative techniques
+ID: 1, Score: 0.0952, Text: New makeup trends focus on bold colors and innovative techniques
 ID: 3, Score: 0.0825, Text: Neue Make-up-Trends setzen auf kräftige Farben und innovative Techniken
-ID: 7, Score: 0.0583, Text: 新的化妆趋势注重鲜艳的颜色和创新的技巧
+ID: 7, Score: 0.0593, Text: 新的化妆趋势注重鲜艳的颜色和创新的技巧
+"""
+
+# 使用 bge-reranker-v2-m3 模型
+"""
+Query: Organic skincare products for sensitive skin
+ID: 8, Score: 0.9988, Text: 敏感肌のために特別に設計された天然有機スキンケア製品
+ID: 6, Score: 0.9970, Text: 针对敏感肌专门设计的天然有机护肤产品
+ID: 0, Score: 0.9882, Text: Organic skincare for sensitive skin with aloe vera and chamomile.
+ID: 4, Score: 0.9786, Text: Cuidado de la piel orgánico para piel sensible con aloe vera y manzanilla
+ID: 2, Score: 0.7314, Text: Bio-Hautpflege für empfindliche Haut mit Aloe Vera und Kamille
+ID: 9, Score: 0.0000, Text: 新しいメイクのトレンドは鮮やかな色と革新的な技術に焦点を当てています
+ID: 7, Score: 0.0000, Text: 新的化妆趋势注重鲜艳的颜色和创新的技巧
+ID: 5, Score: 0.0000, Text: Las nuevas tendencias de maquillaje se centran en colores vivos y técnicas innovadoras
+ID: 1, Score: 0.0000, Text: New makeup trends focus on bold colors and innovative techniques
+ID: 3, Score: 0.0000, Text: Neue Make-up-Trends setzen auf kräftige Farben und innovative Techniken
 """
